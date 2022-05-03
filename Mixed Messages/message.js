a8b450901db4be4a3ec95292cd00ea157c23be8c
rosecs <32778331+rosecs@users.noreply.github.com>
2022-05-02 20:19:52 -0500

    NEW: Create the homepage to Nespresso, a coffee shop

--- Mixed Messages/message.js
+++ Mixed Messages/message.js
@@ -1,7 +1,12 @@
 // a factory function that accepts "articles", "subjects",
 // "nouns", "verbs","adjetives", "conectors", "questions",
 // "signs" and "numbers" parameters to return a customized
-// message constructor with random words
+//  horoscope constructor with random words
+
+let zodiacSigns = ['Capricorn', 'Aquarius', 'Leo', 'Pisces',
+    'Saggitarius', 'Scorpio', 'Libra', 'Virgo', 'Leo',
+'Cancer','Gemini', 'Taurus','Aries'];
+
 const messageBuilder = () => {
     return {
         articles: ["the", "a", "an"],
@@ -12,17 +17,45 @@ const messageBuilder = () => {
         nouns: {
             animals: ["cat", "dog", "horse", "fish", "bear", "cow", "pig", "elephant"],
             home: ["kitchen", "garden", "office", "room", "bath", "bedroom"],
-            places: ["beach", "city", "park", "office", "forest", "house"],
+            places: [" at the beach", "at the city", "at the park", "at the office", "at work", "forest", "house"],
             cities: ["Mexico", "Paris", "New York", "Oslo", "Okinawa", "Bali", "Berlin", "Dublin"]
         },
         verbs: ["run", "watch", "cook", "see", "draw", "paint", "travel", "go"],
-        adjetives: ["pretty", "good", "bad"],
+        futureVerbs: ["will be", "could be", "may be"],
+        adjetives: ["pretty", "good", "bad", "energetic", "busy", "relaxed","playful"],
         connectors: ["with", "but"],
         questions: ["what", "where", "why",],
         signs: [" ", ".", ":", ";", ",", "!", "?"],
         numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        weeklyHoroscope: function() {
+            return this.futureVerbs +
+        },
     }
 }
 
+// Iterate over the object considering the array length
+  for(let item in collectiveAdvice) {
+    let optionIdx = generateRandomNumber(collectiveAdvice[item].length)
+  
+    // use the object's properties to customize the message being added to collectiveAdvice 
+    switch(item) {
+      case 'programLanguage':
+        nextStep.push(`Your skill path right now is a "${collectiveAdvice[item][optionIdx]}".`)
+        break
+      case 'level':
+        nextStep.push(`Your next target should be: "${collectiveAdvice[item][optionIdx]}".`)
+        break
+      case 'tip':
+        nextStep.push(`You should: "${collectiveAdvice[item][optionIdx]}".`)
+        break
+      default:
+        nextStep.push('There is not enough info.')
+    }
+  }
+
+//future + adjetives + places
 
 
+let zodiacMessage = "You" + "this week. You";
+
+console.log(zodiacMessage);
